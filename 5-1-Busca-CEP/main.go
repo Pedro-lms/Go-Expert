db11c6d2f2f9f3631f3ae2375cfc3f5f60586f0c
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-03-05 18:09:31 -0300

    Joining knowladges and creating am address code validator for json struct

---
+++ 5-1-Busca-CEP/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+
+	for _, cep := range os.Args[1:] {
+		req, err := http.Get("http://viacep.com.br/ws/" + cep + "/json")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error getting request: %e\n", err)
+		}
+		defer req.Body.Close()
+
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading response: %e\n", err)
+		}
+
+		var data ViaCEP
+		err = json.Unmarshal(res, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing response: %e\n", err)
+		}
+		fmt.Println(data)
+		fmt.Printf(data.Localidade)
+
+		file, err := os.Create("cidade.txt")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating file: %e\n", err)
+		}
+		defer file.Close()
+
+		//Criando um arquivo para o que for procurado
+		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", data.Cep, data.Localidade, data.Uf))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing to file: %e\n", err)
+		}
+		fmt.Println("Arquivo criado com sucesso")
+		fmt.Println("Cidade: ", data.Localidade)
+	}
+}
