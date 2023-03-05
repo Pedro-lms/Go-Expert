9d5fa2adacda7a75038590108aabd2cfc8c40fdd
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-03-05 11:57:28 -0300

    Json

---
+++ 4-Json/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Conta struct {
+	Numero int
+	Saldo  int
+}
+
+func main() {
+	conta := Conta{Numero: 1, Saldo: 10000}
+	res, err := json.Marshal(conta)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(res))
+
+	err = json.NewEncoder(os.Stdout).Encode(conta)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonPuro := []byte(`{"Numero":2,"Saldo":5000}`)
+	var contaX Conta
+	err = json.Unmarshal(jsonPuro, &contaX)
+	if err != nil {
+		panic(err)
+	}
+	println(contaX.Numero)
+	println(contaX.Saldo)
+}
