d6f203ce636490616644c3d8795148a6fc6b3525
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-03-11 14:52:56 -0300

    http changes

--- 5-Busca-CEP/main.go
+++ 5-Busca-CEP/main.go
@@ -1,58 +1,25 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
-	"os"
 )
 
-type ViaCEP struct {
-	Cep         string `json:"cep"`
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	Uf          string `json:"uf"`
-	Ibge        string `json:"ibge"`
-	Gia         string `json:"gia"`
-	Ddd         string `json:"ddd"`
-	Siafi       string `json:"siafi"`
+func main() {
+	http.HandleFunc("/", BuscaCepHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
-func main() {
-
-	for _, url := range os.Args[1:] {
-		req, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error getting request: %e\n", err)
-		}
-		defer req.Body.Close()
-
-		res, err := io.ReadAll(req.Body)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading response: %e\n", err)
-		}
-
-		var data ViaCEP
-		err = json.Unmarshal(res, &data)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error parsing response: %e\n", err)
-		}
-		fmt.Println(data)
-		fmt.Printf(data.Localidade)
-
-		file, err := os.Create("cidade.txt")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error creating file: %e\n", err)
-		}
-		defer file.Close()
-
-		//Criando um arquivo para o que for procurado
-		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", data.Cep, data.Localidade, data.Uf))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing to file: %e\n", err)
-		}
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
 	}
+	cepParams := r.URL.Query().Get("cep")
+	if cepParams == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(r.URL.Path))
 }
