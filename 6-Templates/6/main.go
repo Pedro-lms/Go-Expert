96490c7a5b34ff20441fea3c852a2cb7365f81fe
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-03-12 23:02:36 -0300

    Strings

---
+++ 6-Templates/6/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+func main() {
+	templates := []string{
+		"header.html",
+		"content.html",
+		"footer.html",
+	}
+
+	t := template.New("content.html")
+	t.Funcs(template.FuncMap{"ToUpper": ToUpper})
+	t = template.Must(t.ParseFiles(templates...))
+	err := t.Execute(os.Stdout, Cursos{
+		Curso{Nome: "C#", CargaHoraria: 10},
+		Curso{Nome: "Go", CargaHoraria: 20},
+	})
+	if err != nil {
+		panic(err)
+	}
+	http.ListenAndServe(":8282", nil)
+}
