89fe6cb941e37ea1bf5500c3b0b4b75fd85a626e
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-04-23 21:16:53 -0300

    build(Packing and testing it): up: errors from import entity

---
+++ 9-APIs/internal/entity/user_test.go
@@ -0,0 +1,18 @@
+package entity
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("Pedro", "Pedro@gmail.com", "123")
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, "Pedro", user.Name)
+	assert.Equal(t, "Pedro@gmail.com", user.Email)
+}
+
+func TestUser_ValidatePassword(t *testing.T) {
+	user, err := NewUser("Pedro", "Pedro@gmail.com", "123")
+	assert.Nil(t, err)
+	assert.True(t, user.ValidatePassword("123"))
+	assert.False(t, user.ValidatePassword("12"))
+}
