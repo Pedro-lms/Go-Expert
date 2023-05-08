2289ce41d2701085d3f1f42b9d85aa7a6c1529a8
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-05-07 21:37:32 -0300

    Find all with pagination

--- 9-APIs/internal/entity/infra/database/product_db.go
+++ 9-APIs/internal/entity/infra/database/product_db.go
@@ -17,6 +17,21 @@ func (p *Product) Create(product *entity.Product) error {
 	return p.Db.Create(product).Error
 }
 
+func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error) {
+	var products []entity.Product
+	var err error
+	if sort != "" && sort != "asc" && sort != "desc" {
+		sort = "asc"
+	}
+	if page != 0 && limit != 0 {
+		err = p.Db.Limit(limit).Offset((page - 1) * limit).
+			Order("created_at" + sort).Find(&products).Error
+	} else {
+		err = p.Db.Order("created_at" + sort).Find(&products).Error
+	}
+	return products, err
+}
+
 func (p *Product) FindById(id string) (*entity.Product, error) {
 	var product entity.Product
 	err := p.Db.First(&product, "id = ?", id).Error
