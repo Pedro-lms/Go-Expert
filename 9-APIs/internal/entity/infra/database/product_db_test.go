ff9e60d24d2be763a472df91e51034297f064eb0
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-05-11 19:14:29 -0300

    product test

---
+++ 9-APIs/internal/entity/infra/database/product_db_test.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/Pedro-lms/goexpert/9-APIs/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateNewProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 10.00)
+	assert.NoError(t, err)
+	productDB := NewProduct(db)
+	err = productDB.Create(product)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, product.ID)
+}
+
+func TestFindAllProducts(t *testing.T) {
+
+	db, err := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
+	defer func() {
+		dbSQL, _ := db.DB()
+		dbSQL.Close()
+	}()
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	for i := 1; i < 24; i++ {
+		product, err := entity.NewProduct(fmt.Sprintf("Product %d", i), rand.Float64()*100)
+		assert.NoError(t, err)
+		productDB := NewProduct(db)
+		err = productDB.Create(product)
+	}
+}
