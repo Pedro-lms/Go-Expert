a443034938779b933ec4687e0511e61bb3f91809
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-08-07 21:36:26 -0300

    finishing test product

--- 9-APIs/internal/entity/infra/database/product_db_test.go
+++ 9-APIs/internal/entity/infra/database/product_db_test.go
@@ -27,7 +27,7 @@ func TestCreateNewProduct(t *testing.T) {
 
 func TestFindAllProducts(t *testing.T) {
 
-	db, err := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
 	defer func() {
 		dbSQL, _ := db.DB()
 		dbSQL.Close()
@@ -41,5 +41,61 @@ func TestFindAllProducts(t *testing.T) {
 		assert.NoError(t, err)
 		productDB := NewProduct(db)
 		err = productDB.Create(product)
+		if err != nil {
+			panic(err)
+		}
 	}
+
+	productDb := NewProduct(db)
+	products, err := productDb.FindAll(1, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "Product 1", products[0].Name)
+	assert.Equal(t, "Product 10", products[9].Name)
+
+	products, err = productDb.FindAll(2, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "Product 11", products[0].Name)
+	assert.Equal(t, "Product 20", products[9].Name)
+
+	products, err = productDb.FindAll(3, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "Product 21", products[0].Name)
+	assert.Equal(t, "Product 30", products[9].Name)
+}
+
+func TestUpdateProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+	product.Name = "Product2"
+	err = productDB.Update(product)
+	product, err = productDB.FindById((product.ID.String()))
+	assert.NoError(t, err)
+	assert.Equal(t, "Product2", product.Name)
+}
+
+func TestDeleteProdut(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product1", 10.00)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+	product.Name = "Product2"
+	err = productDB.Delete(product.ID.String{})
+	assert.NoError(t, err)
+	product, err = productDB.FindById((product.ID.String()))
+	assert.Equal(t, "Product2", product.Name)
 }
